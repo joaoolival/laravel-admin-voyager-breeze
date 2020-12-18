7357c4ece4730ceeede903b7c16b9c8b3a17f79b
joaoolival <joaoljolival@gmail.com>
2020-12-18 04:04:44 +0000

    first commit

---
+++ database/migrations/2016_01_01_000000_add_voyager_admin_fields.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddVoyagerAdminFields extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('admins', function ($table) {
+            if (!Schema::hasColumn('admins', 'avatar')) {
+                $table->string('avatar')->nullable()->after('email')->default('admins/default.png');
+            }
+            $table->bigInteger('role_id')->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('admins', 'avatar')) {
+            Schema::table('admins', function ($table) {
+                $table->dropColumn('avatar');
+            });
+        }
+        if (Schema::hasColumn('admins', 'role_id')) {
+            Schema::table('admins', function ($table) {
+                $table->dropColumn('role_id');
+            });
+        }
+    }
+}
