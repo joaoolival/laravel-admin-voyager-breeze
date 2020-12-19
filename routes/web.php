0048f6c893dba48fff316d8dff6f3c33fe7c0a53
joaoolival <joaoljolival@gmail.com>
2020-12-19 16:33:22 +0000

    email verify

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
